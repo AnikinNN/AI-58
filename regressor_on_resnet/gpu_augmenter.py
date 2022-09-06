normalization fix, accuracy analysis added

regressor_on_resnet/gpu_augmenter.py
@@ -13,33 +13,50 @@ class Augmenter:
         transforms.Normalize((-0.485, -0.456, -0.406), (1., 1., 1.)),
     ])
 
-    @staticmethod
-    def sampler(delta: float = 1, center: float = 0):
-        return (np.random.rand() - 0.5) * 2 * delta + center
+    class Sampler:
+        def __init__(self):
+            self.rotation_angle = self.sampler(5)
+            self.shear_x = self.sampler(5)
+            self.shear_y = self.sampler(5)
+            self.flip_ud = self.sampler(0.5, 0.5)
+            self.flip_lr = self.sampler(0.5, 0.5)
+            self.brightness_multiplier = self.sampler(0.02, 1)
+            self.brightness_delta = self.sampler(0.02)
+
+        @staticmethod
+        def sampler(delta: float = 1, center: float = 0):
+            return (np.random.rand() - 0.5) * 2 * delta + center
 
     @classmethod
-    def augment(cls, images: torch.Tensor, is_mask: bool):
+    def augment_image(cls, images: torch.Tensor, is_mask: bool, sampler: Sampler):
 
-        images = transforms.functional.affine(images, cls.sampler(45), [0, 0], 1,
-                                              [cls.sampler(5), cls.sampler(5)],
+        images = transforms.functional.affine(images, sampler.rotation_angle, [0, 0], 1,
+                                              [sampler.shear_x, sampler.shear_y],
                                               interpolation=transforms.InterpolationMode.NEAREST)
 
-        if cls.sampler(0.5, 0.5) > 0.5:
-            images = torch.flip(images, dims=[1])
-        if cls.sampler(0.5, 0.5) > 0.5:
+        if sampler.flip_ud > 0.5:
             images = torch.flip(images, dims=[2])
+        if sampler.flip_lr > 0.5:
+            images = torch.flip(images, dims=[3])
 
         if not is_mask:
-            images = transforms.functional.adjust_brightness(images, cls.sampler(0.15, 1))
-            images = (images + cls.sampler(0.2)).clamp(0, 1.0)
-            images = cls.normalizer(images)
+            images = transforms.functional.adjust_brightness(images, sampler.brightness_multiplier)
+            images = (images + sampler.brightness_delta).clamp(0, 1.0)
         return images
 
     @classmethod
+    def augment_elevation(cls, elevation: torch.Tensor):
+        elevation_augmented = torch.clip(torch.normal(elevation, 1.0), -90, 90)
+        elevation_sin = torch.sin(torch.deg2rad(elevation_augmented))
+        return elevation_sin
+
+    @classmethod
     def __call__(cls, batch: FluxBatch):
-        images = cls.augment(batch.images, False)
-        masks = cls.augment(batch.masks, True)
-        return images * masks
+        sampler = cls.Sampler()
+        images = cls.augment_image(batch.images, False, sampler)
+        masks = cls.augment_image(batch.masks, True, sampler)
+        elevations = cls.augment_elevation(batch.elevations)
+        return images, masks, elevations
 
     @classmethod
     def call(cls, batch: FluxBatch):

regressor_on_resnet/resnet_regressor.py
@@ -14,13 +14,14 @@ class ResnetRegressor(torch.nn.Module):
 
         # todo in_features=2049 replace by something like
         # self.resnet.fc.output_shape + 1
-        widths = [2049]
-        widths.extend(widths)
-        widths.append(1)
+        self.widths = [2049]
+        self.widths.extend(widths)
+        self.widths.append(1)
+
 
         layers = []
-        for i in range(len(widths) - 1):
-            layers.append(nn.Linear(in_features=widths[i], out_features=widths[i + 1]))
+        for i in range(len(self.widths) - 1):
+            layers.append(nn.Linear(in_features=self.widths[i], out_features=self.widths[i + 1]))
             layers.append(Mish())
 
         self.fully_connected = torch.nn.Sequential(*layers)

regressor_on_resnet/train_common.py
@@ -1,5 +1,9 @@
+import traceback
+import logging
 import warnings
 
+import numpy as np
+import os
 import pandas as pd
 import torch
 from torch.nn import MSELoss
@@ -12,11 +16,8 @@ from regressor_on_resnet.nn_logging import Logger
 from regressor_on_resnet.sgdr_restarts_warmup import CosineAnnealingWarmupRestarts
 from regressor_on_resnet.batch_factory import BatchFactory
 from regressor_on_resnet.gpu_augmenter import Augmenter
-
-import numpy as np
-import os
-
 from regressor_on_resnet.weighted_mse import WeightedMse, get_weights_and_bounds
+from regressor_on_resnet.resnet_regressor import ResnetRegressor
 
 
 def train_single_epoch(model: torch.nn.Module,
@@ -46,7 +47,7 @@ def train_single_epoch(model: torch.nn.Module,
 
         optimizer.zero_grad()
         data_out = model(batch.images, batch.elevations)
-        loss = loss_function(data_out, batch.fluxes, batch.hard_mining_weights)
+        loss = loss_function(data_out, batch.fluxes)
         loss_values.append(loss.item())
 
         loss_tb.append(loss.item())
@@ -72,6 +73,7 @@ def train_single_epoch(model: torch.nn.Module,
 
 def validate_single_epoch(model: torch.nn.Module,
                           loss_function: torch.nn.Module,
+                          quality_function: torch.nn.Module,
                           cuda_batches_queue: Queue,
                           per_step_epoch: int,
                           current_epoch: int,
@@ -79,6 +81,7 @@ def validate_single_epoch(model: torch.nn.Module,
                           ):
     model.eval()
     loss_values = []
+    qualities = []
 
     pbar = tqdm(total=per_step_epoch, ncols=100)
     pbar.set_description(desc='validation')
@@ -95,11 +98,14 @@ def validate_single_epoch(model: torch.nn.Module,
 
         loss = loss_function(data_out, batch.fluxes)
         loss_values.append(loss.item())
+
+        quality = quality_function(data_out, batch.fluxes)
+        qualities.append(quality.item())
         pbar.update()
         pbar.set_postfix({'loss': loss.item(), 'cuda_queue_len': cuda_batches_queue.qsize()})
     pbar.close()
 
-    return np.mean(loss_values)
+    return np.mean(loss_values), np.mean(qualities)
 
 
 def calculate_hard_mining_weights(model: torch.nn.Module,
@@ -135,7 +141,7 @@ def calculate_hard_mining_weights(model: torch.nn.Module,
     return
 
 
-def train_model(model: torch.nn.Module,
+def train_model(model: ResnetRegressor,
                 train_dataset: FluxDataset,
                 val_dataset: FluxDataset,
                 hard_mining_dataset: FluxDataset,
@@ -147,10 +153,11 @@ def train_model(model: torch.nn.Module,
                 use_warmup: bool = False,
                 steps_per_epoch_train=1536,
                 steps_per_epoch_valid=1024,
+                train_convolutional_since_epoch=None,
                 ):
     mse = MSELoss()
-    weights, bounds = get_weights_and_bounds(train_dataset.flux_frame['CM3up[W/m2]'].to_numpy())
-    weighted_mse = WeightedMse(weights, bounds)
+    # weights, bounds = get_weights_and_bounds(train_dataset.flux_frame['CM3up[W/m2]'].to_numpy())
+    # weighted_mse = WeightedMse(weights, bounds)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     # lr_scheduler.step() calls every batch
     if static_learning_rate:
@@ -175,11 +182,11 @@ def train_model(model: torch.nn.Module,
                                             do_augment=False,
                                             preprocess_worker_number=15,
                                             to_variable=False)
-    hard_mining_batch_factory = BatchFactory(hard_mining_dataset,
-                                             cuda_device,
-                                             do_augment=False,
-                                             preprocess_worker_number=15,
-                                             to_variable=False)
+    # hard_mining_batch_factory = BatchFactory(hard_mining_dataset,
+    #                                          cuda_device,
+    #                                          do_augment=False,
+    #                                          preprocess_worker_number=15,
+    #                                          to_variable=False)
 
     best_val_loss = float('Inf')
     best_val_epoch = -1
@@ -187,9 +194,15 @@ def train_model(model: torch.nn.Module,
     try:
         for epoch in range(max_epochs):
             print(f'Epoch {epoch} / {max_epochs}')
+
+            # enable training of convolutional part at desired epoch
+            if train_convolutional_since_epoch is not None and train_convolutional_since_epoch == epoch:
+                print('train convolutional on')
+                model.set_train_convolutional_part(True)
+
             train_loss = train_single_epoch(model,
                                             optimizer,
-                                            weighted_mse,
+                                            mse,
                                             train_batch_factory.cuda_queue,
                                             steps_per_epoch_train,
                                             current_epoch=epoch,
@@ -197,42 +210,45 @@ def train_model(model: torch.nn.Module,
                                             lr_scheduler=lr_scheduler)
             logger.tb_writer.add_scalar('train_loss_per_epoch', train_loss, epoch)
 
-            val_loss = validate_single_epoch(model,
-                                             mse,
-                                             validation_batch_factory.cuda_queue,
-                                             steps_per_epoch_valid,
-                                             current_epoch=epoch,
-                                             logger=logger)
-            logger.tb_writer.add_scalar('val_loss', val_loss, epoch)
-            print(f'Validation loss: {val_loss}')
+            val_weighted_mse, val_mse = validate_single_epoch(model,
+                                                              mse,
+                                                              mse,
+                                                              validation_batch_factory.cuda_queue,
+                                                              steps_per_epoch_valid,
+                                                              current_epoch=epoch,
+                                                              logger=logger)
+            logger.tb_writer.add_scalar('val_mse', val_mse, epoch)
+            logger.tb_writer.add_scalar('val_weighted_mse', val_weighted_mse, epoch)
+            print(f'Validation loss: {val_mse}')
 
-            if best_val_loss > val_loss:
+            if best_val_loss > val_mse:
                 # save new model
                 torch.save(model, os.path.join(logger.misc_dir, f'model_ep{epoch}.pt'))
                 # delete old model
                 old_model_path = os.path.join(logger.misc_dir, f'model_ep{best_val_epoch}.pt')
                 if os.path.exists(old_model_path):
                     os.remove(old_model_path)
-                best_val_loss = val_loss
+                best_val_loss = val_mse
                 best_val_epoch = epoch
 
             # every single pass of whole dataset, not at epoch == 0
-            if epoch % int(len(train_dataset) / train_dataset.batch_size / steps_per_epoch_train + 1) == 0 and epoch:
-                calculate_hard_mining_weights(model,
-                                              train_dataset,
-                                              hard_mining_dataset,
-                                              hard_mining_batch_factory.cuda_queue,
-                                              logger,
-                                              epoch)
+                # if epoch % int(len(train_dataset) / train_dataset.batch_size / steps_per_epoch_train + 1) == 0 and epoch:
+                #     calculate_hard_mining_weights(model,
+                #                                   train_dataset,
+                #                                   hard_mining_dataset,
+                #                                   hard_mining_batch_factory.cuda_queue,
+                #                                   logger,
+                #                                   epoch)
 
     except KeyboardInterrupt:
         pass
 
-    for i in (
-            train_batch_factory,
-            hard_mining_batch_factory,
-            validation_batch_factory,):
-        i.stop()
+    finally:
+        for i in (
+                train_batch_factory,
+                # hard_mining_batch_factory,
+                validation_batch_factory,):
+            i.stop()
 
     print('train_model: done')
     return best_val_loss