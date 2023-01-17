eb360f7ad21adef9618541c88f053611e8421e8f
Vasilisa Koshkina <anikin.nn@phystech.edu>
2023-01-17 22:02:41 +0000

    cleanup

--- regressor_on_resnet/batch.py
+++ regressor_on_resnet/batch.py
@@ -82,19 +82,6 @@ class FluxBatch:
 
         self.state = FluxBatchState.CPU_STORING
 
-    def _to_tensor(self):
-        if self.state is not FluxBatchState.CPU_APPENDING:
-            raise ValueError(f'You can convert to tensor only from FluxBatchState.CPU_APPENDING state. '
-                             f'But there was an attempt on {self.state} state')
-
-        self.images = torch.stack(tuple(torch.tensor((i / 255.).transpose(2, 0, 1)) for i in self.images))
-        self.masks = torch.stack(tuple(torch.tensor(i.transpose(2, 0, 1)) for i in self.masks))
-        self.elevations = torch.reshape(torch.tensor(self.elevations), (-1, 1))
-        self.fluxes = torch.reshape(torch.tensor(self.fluxes), (-1, 1))
-        self.hard_mining_weights = np.array(self.hard_mining_weights)
-
-        self.state = FluxBatchState.CPU_STORING
-
     def to_cuda(self, to_variable: bool, cuda_device: torch.device) -> None:
         if self.state is not FluxBatchState.CPU_STORING:
             raise ValueError(f'You can load to cuda device only on FluxBatchState.CPU_STORING state. '
@@ -108,28 +95,5 @@ class FluxBatch:
                     attribute = Variable(attribute)
                 attribute = attribute.to(cuda_device)
                 self.__setattr__(i, attribute)
-
-        self.state = FluxBatchState.CUDA_STORING
-
-    def _to_cuda(self, cuda_device, to_variable: bool):
-        if self.state is not FluxBatchState.CPU_STORING:
-            raise ValueError(f'You can load to cuda device only on FluxBatchState.CPU_STORING state. '
-                             f'But there was an attempt on {self.state} state')
-
-        self.fluxes = self.fluxes.float()
-        self.images = self.images.float()
-        self.masks = self.masks.float()
-        self.elevations = self.elevations.float()
-
-        if to_variable:
-            self.fluxes = Variable(self.fluxes)
-            self.images = Variable(self.images)
-            self.masks = Variable(self.masks)
-            self.elevations = Variable(self.elevations)
-
-        self.fluxes = self.fluxes.to(cuda_device)
-        self.images = self.images.to(cuda_device)
-        self.masks = self.masks.to(cuda_device)
-        self.elevations = self.elevations.to(cuda_device)
 
         self.state = FluxBatchState.CUDA_STORING
